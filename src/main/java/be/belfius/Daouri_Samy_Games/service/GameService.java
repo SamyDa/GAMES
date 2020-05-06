6d5bd65f5aa9d49ca443b5e9c4730a48fae5e7c3
samyd <samyd@192.168.1.15>
2020-05-06 10:48:03 +0200

    06/05/2020

---
+++ src/main/java/be/belfius/Daouri_Samy_Games/service/GameService.java
@@ -0,0 +1,19 @@
+package be.belfius.Daouri_Samy_Games.service;
+
+import be.belfius.Daouri_Samy_Games.repository.GameRepository;
+
+public class GameService {
+	
+	private GameRepository gameRepository = new GameRepository();
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
